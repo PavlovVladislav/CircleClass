5d2be4e890393415bee698633549505efcaa3839
Bragi <beluerosy@gmail.com>
2019-10-09 23:40:01 +0300

    Create Documentation

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circle"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Elipse"}]
